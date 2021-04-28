c369a4193fa51ff92cc74b667b9d5d44f580dfa0
VahePashayan <vpashayan39gmail.com>
2021-04-28 22:39:42 +0400

    added and solved the task of the chapter

---
+++ HW3/ComparisonsChapter.js
@@ -0,0 +1,7 @@
+console.log(5 > 4) //true
+console.log("apple" > "pineapple") //false as "a" < "P"
+console.log("2" > "12") //true as "2" > "1"
+console.log(undefined == null) //true as both of them are false
+console.log(undefined === null) //false as their types are different
+console.log(null == "\n0\n") // false as null only equals undefined
+console.log(null === +"\n0\n") // false as their types are different
