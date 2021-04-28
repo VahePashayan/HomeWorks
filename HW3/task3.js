9c4ef3eca0cc60cdbfade87a1f48316f3a21d260
VahePashayan <vpashayan39gmail.com>
2021-04-28 17:06:17 +0400

    solved the task

--- HW3/task3.js
+++ HW3/task3.js
@@ -3,9 +3,9 @@ b = 3;
 // a = 3, b = 10
 
 let c = 0;
-c = a; //c = 3, a = 3, b = 10
-a = b; //c = 3, a = 10, b = 10
-b = c; //c = 3, a = 10, b = 3
+c = a; //c = 10, a = 10, b = 3
+a = b; //c = 10, a = 3, b = 3
+b = c; //c = 10, a = 3, b = 10
 
 console.log(a);
 console.log(b);
