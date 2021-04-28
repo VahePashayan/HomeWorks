4294d3adca9cb0e0ddcd0838c11583420c3f2064
VahePashayan <vpashayan39gmail.com>
2021-04-28 17:01:56 +0400

    solved the task

--- HW3/task3.js
+++ HW3/task3.js
@@ -1,1 +1,11 @@
-console.log("1 + 2");
+let a = 10, 
+b = 3;
+// a = 3, b = 10
+
+let c = 0;
+c = a; //c = 3, a = 3, b = 10
+a = b; //c = 3, a = 10, b = 10
+b = c; //c = 3, a = 10, b = 3
+
+console.log(a);
+console.log(b);
