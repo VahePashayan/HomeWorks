18f95d448343bb90341dbe6790ba696916c95306
VahePashayan <vpashayan39gmail.com>
2021-04-29 23:36:41 +0400

    Task about replacement of the words

---
+++ HW4/ReplaceWithTheWordsFromArray.js
@@ -0,0 +1,13 @@
+let sen = "_, we have a _."
+let arr = ["Houston", "problem"];
+
+let arrSen = sen.split(" ");
+for(let i = 0; i <= arrSen.length - 1; i++) {
+    for(let j = 0; j <= arr.length - 1; j++) {
+        if(arrSen[i].includes("_")) {
+            arrSen[i] = arr[j];
+            arr.shift();
+        }
+    }    
+}
+console.log(arrSen.join(" "));
