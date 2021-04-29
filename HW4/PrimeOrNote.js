fa34fb5c623d142ecdd2aac62c4f6fdbfe6da56b
VahePashayan <vpashayan39gmail.com>
2021-04-29 18:13:17 +0400

    Task about prime number

---
+++ HW4/PrimeOrNote.js
@@ -0,0 +1,16 @@
+let num = 401;
+let isPrime = true;
+
+for(let i = 2; i < num; i++) {
+    if(num % i === 0) {
+        isPrime = false;
+        break;
+    }  
+}
+
+if(isPrime) {
+    console.log("YES")
+}else{
+    console.log("NO")
+}
+
