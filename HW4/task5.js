20a9f44dd0d94a7cab99917d38adada75ada89c3
VahePashayan <vpashayan39gmail.com>
2021-04-29 14:18:23 +0400

    solved the task about the multiple of 3, 5 and 7

---
+++ HW4/task5.js
@@ -0,0 +1,25 @@
+let num = 42;
+
+if(num % 3 === 0) {
+    console.log(num + " is a multiple of 3");
+}
+else if(num % 5 === 0) {
+    console.log(num + " is a multiple of 5");
+}
+else if(num % 7 === 0) {
+    console.log(num + " is a multiple of 7");
+}
+else if(num % 3 === 0 && num % 5 === 0) {
+    console.log(num + " is a multiple of 3 and 5");
+}
+else if(num % 3 === 0 && num % 7 === 0) {
+    console.log(num + " is a multiple of 3 and 7");
+}
+else if(num % 5 === 0 && num % 7 === 0) {
+    console.log(num + " is a multiple of 5 and 7");
+}
+else if(num % 3 === 0 && num % 5 === 0 && num % 7 === 0) {
+    console.log(num + " is a multiple of 3, 5 and 7");
+} else {
+    console.log(num + " is not a multiple of 3, 5, or 7")
+}
