b9f083f4283e37b227d6962bdc99dc005ef20f77
VahePashayan <vpashayan39gmail.com>
2021-04-29 12:58:10 +0400

    added and solved task about even and odd numbers

--- HW4/task1.js
+++ HW4/task1.js
@@ -1,4 +1,4 @@
-let arr = [1, 2, 3, 4 , 5 , 6];
+let arr = [1, 2, 3, 4, 5, 6];
 let i = 0;
 while (i < arr.length) {
     console.log("for", i, arr[i]);
@@ -10,4 +10,4 @@ let j = 0;
 do {
     console.log("for", j, arr1[j])
     j++;
-} while(j < arr1.length);
+} while (j < arr1.length);
