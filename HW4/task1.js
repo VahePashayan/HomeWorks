90f8e5ec08de57c0ad26087aeb51066f617970f2
VahePashayan <vpashayan39gmail.com>
2021-04-29 12:52:59 +0400

    added and solved fist task about while and do while

---
+++ HW4/task1.js
@@ -0,0 +1,13 @@
+let arr = [1, 2, 3, 4 , 5 , 6];
+let i = 0;
+while (i < arr.length) {
+    console.log("for", i, arr[i]);
+    i++;
+}
+
+let arr1 = [1, 2, 3, 4, 5, 6];
+let j = 0;
+do {
+    console.log("for", j, arr1[j])
+    j++;
+} while(j < arr1.length);
