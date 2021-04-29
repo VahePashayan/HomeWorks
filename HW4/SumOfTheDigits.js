e531933e7409e541d7b7aa368afa695f930b5fb2
VahePashayan <vpashayan39gmail.com>
2021-04-29 22:29:03 +0400

    added the task about sum and product of the number's digits

---
+++ HW4/SumOfTheDigits.js
@@ -0,0 +1,17 @@
+let num = 455;
+let str = num + "";
+let sum = 0;
+let prod = 1;
+
+for(let i = 0; i <= str.length -  1; i++) {
+    sum = sum + (+str[i]);
+    prod = prod * str[i];
+}
+
+if(prod % sum === 0){
+    console.log("Quotient is " + prod/sum + ".");
+}else if (prod === 0) {
+    console.log("Cannot calculate.");
+}else {
+    console.log("Remainder is " + prod % sum + ".");
+}
