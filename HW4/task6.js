231cc0ec89e24e0db4ac2e278b5eafd10c60499b
VahePashayan <vpashayan39gmail.com>
2021-04-29 15:31:15 +0400

    task about ascending sort

---
+++ HW4/task6.js
@@ -0,0 +1,37 @@
+let a = 20293847;
+let b = 15845345;
+let c = 23458;
+
+if(a < b && b < c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+if(a < c && c < b) {
+    console.log(a);
+    console.log(c);
+    console.log(b);
+}
+if(b < a && a < c) {
+    console.log(b);
+    console.log(a);
+    console.log(c);
+}
+if(b < c && c < a) {
+    console.log(b);
+    console.log(c);
+    console.log(a);
+}
+if(c < a && a < b) {
+    console.log(c);
+    console.log(a);
+    console.log(b);
+}
+if(c < b && b < a) {
+    console.log(c);
+    console.log(b);
+    console.log(a);
+}
+
+
+
