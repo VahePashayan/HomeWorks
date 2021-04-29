06d2e2b030c50a7a4f9a825dbeba65e9ee864e85
VahePashayan <vpashayan39gmail.com>
2021-04-29 16:38:11 +0400

    Digit in the number

---
+++ HW4/digitContainsNumber.js
@@ -0,0 +1,15 @@
+let digit = 1;
+let num = 8763425;
+
+let numStr = num + "";
+let digitStr = digit + "";
+
+if(numStr.indexOf(digitStr) === -1) {
+    console.log("NO")
+}else {
+    console.log("YES")
+}
+
+
+
+
