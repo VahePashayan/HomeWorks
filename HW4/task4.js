cce256c88b6fd7b22aa843143da8bfbd034b2f65
VahePashayan <vpashayan39gmail.com>
2021-04-29 13:56:05 +0400

    added and solved the task about

---
+++ HW4/task4.js
@@ -0,0 +1,17 @@
+// Bring the last digit of the number to the beginning
+let int = 12345;
+let last;
+let str;
+
+if(int % 10 === 0){
+    console.log(int);
+} else {
+    str = int.toString();
+    last = str[str.length - 1];
+    console.log(last + str.slice(0, str.length - 1))
+}
+
+
+
+
+
