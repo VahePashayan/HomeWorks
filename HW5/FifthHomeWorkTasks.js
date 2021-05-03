5add08313b464ac7393a686d079a9636478e23db
VahePashayan <vpashayan39gmail.com>
2021-05-03 13:20:30 +0400

    feat: added task about 0 before number

--- HW5/FifthHomeWorkTasks.js
+++ HW5/FifthHomeWorkTasks.js
@@ -3,3 +3,6 @@
 
 //2 
  console.log(Number('077') === 077)     // false        As Number('077') returns 77, and 077 returns 63
+
+//3 
+console.log(Number(' 12'))      //12        Number() deletes 0 before number
