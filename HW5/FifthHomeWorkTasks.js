fd364dce3728ac52bbaed422da54e15057c85d26
VahePashayan <vpashayan39gmail.com>
2021-05-03 13:16:57 +0400

    feat: added and described the second task

--- HW5/FifthHomeWorkTasks.js
+++ HW5/FifthHomeWorkTasks.js
@@ -1,2 +1,5 @@
 //1
- console.log(Number('0x77') === 0x77)  // true     As number() returns number   
+ console.log(Number('0x77') === 0x77)       // true     As number() returns number   
+
+//2 
+ console.log(Number('077') === 077)     // false        As Number('077') returns 77, and 077 returns 63
