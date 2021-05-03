7179dbf5b3b0acaeb97bd157e2e58e1001efeeef
VahePashayan <vpashayan39gmail.com>
2021-05-03 17:43:05 +0400

    feat: works for both parseInt and parseFloat

---
+++ HW5/parseIntAndParseFloat.js
@@ -0,0 +1,18 @@
+let inputStr = " 2304abc";
+let parsed = "";
+for(let i = 0; i < inputStr.length; i++) {
+    if(inputStr[i].charCodeAt(0) >= 49 && inputStr[i].charCodeAt(0) <= 57 || inputStr[i].charCodeAt(0) === 46) {
+        parsed = parsed + inputStr[i];
+    }else if(inputStr[i] === " " || inputStr[i] === "0") {
+        continue;
+    }else {
+        break;
+    }
+}
+
+if(parsed === "") console.log("NaN");
+if(parsed[0] === ".") {
+    parsed = "0" + parsed;
+}
+console.log(parsed)
+
