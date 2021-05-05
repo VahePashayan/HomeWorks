c5b9d7530348b8a5f46aa5f2945557b202ebb17d
VahePashayan <vpashayan39gmail.com>
2021-05-05 14:59:45 +0400

    feat: added EvenDigit.js file

---
+++ HW6/EvenDigits.js
@@ -0,0 +1,33 @@
+let inputMin = 19;
+let inputMax = 42;
+
+let findArr = function(numMin, numMax) {
+    let arr = [];
+    let res = [];
+    for(i = numMin; i <= inputMax; i++) {
+        if(i % 2 === 0){
+            arr.push(i);
+        }
+    }
+    
+    for(let i = 0; i < arr.length; i++) {
+        arr[i] = arr[i].toString();
+        for(let j = 0; j < arr[i].length; j++) {
+            if(arr[i][j] % 2 === 0 && arr[i][j + 1] % 2 === 0) {
+                res.push(parseInt(arr[i]));
+            }else {
+                break;
+            }
+        }
+    }
+    
+    if(res.length === 0) {
+        return "Such numbers does not exist."
+    }else {
+        return res;
+    }
+    
+}
+
+console.log(findArr(inputMin, inputMax));
+
