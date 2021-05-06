51c5def64c010e8a95f665a4d698839888abe432
VahePashayan <vpashayan39gmail.com>
2021-05-06 13:08:40 +0400

    feat: add first task about closure funcion.

---
+++ HW7/PartialSum.js
@@ -0,0 +1,18 @@
+const getSum = (num) => {
+    return (num2) => {
+        return num + num2;
+    }
+    
+}
+
+console.log(getSum(1)(2));
+
+const addOne = getSum(1);
+console.log(addOne(2));
+
+const addTen = getSum(10);
+console.log(addTen(2));
+
+console.log(addOne(4));
+
+console.log(addTen(10));
