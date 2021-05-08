93862bfa9d1e85aeeafa4e23bc45ce3f27477c51
VahePashayan <vpashayan39gmail.com>
2021-05-08 18:30:50 +0400

    fix: fixed the task about anagram, added the check

--- HW6/AnagramFromArray.js
+++ HW6/AnagramFromArray.js
@@ -2,9 +2,8 @@ let inputWord = "pencil";
 let arrOfWords = ["licnep", "circular", "pupil", "nilcpe", "leppnec"];
 
 function anagrams(word, arr) {
-    const length = word.length;
-    const sample = word.split('').sort().join('');
-    return arr.filter(item => item.length === length)
+    const sample = word.split("").sort().join("");
+    return arr.filter(item => item.split("").sort().join("") === sample)
        
 }
 console.log(anagrams(inputWord, arrOfWords));
