85ac1497c366b45ae40c3156b91af5326d8fe4fa
VahePashayan <vpashayan39gmail.com>
2021-05-11 14:11:03 +0400

    feat: added the task about sorting by ascending and descending order

---
+++ HW10/AscAndDescSort.js
@@ -0,0 +1,11 @@
+let sortBy = function(arr, str) {
+    
+    if(str === "Desc") {
+        return arr.sort((a, b) => b - a);
+    }
+    
+    return arr.sort((a, b) => a - b);
+
+}
+
+console.log(sortBy([4, 3, 2, 54, 101, 1], "Desc" ));
