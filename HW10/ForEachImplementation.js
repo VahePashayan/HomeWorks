7521f6bd3a92434b85ef8950f30da7cb6b3788b5
VahePashayan <vpashayan39gmail.com>
2021-05-11 22:04:06 +0400

    fix: added one more function to script

--- HW10/ForEachImplementation.js
+++ HW10/ForEachImplementation.js
@@ -1,17 +1,21 @@
 let inputArray = [1, 2, "abcd", 3, null, "undefined"];
 
-function doForEach( arr, func) {
-    for(let i = 0; i < arr.length; i++) {
-        const element = arr[i];
-        func(element);
+function doForEach( array, func) {
+    for(let index = 0; index < array.length; index++) {
+        const element = array[index];
+        func(element, index, array);
     }
 };
 
-
+function printArray(el, index, array) {
+    console.log("Index : " + index + " Value : " + array[index]);
+}
 
 function plus10(el) {
     console.log(el + 10);
 };
 
 console.log(doForEach(inputArray, plus10));
+
+console.log(doForEach(inputArray, printArray));
 
