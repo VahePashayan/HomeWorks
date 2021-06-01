74798d35cc5cc5019566769aa4a1fa7d5c46496e
VahePashayan <vpashayan39gmail.com>
2021-06-01 11:39:27 +0400

    feat: added sum and factorial functions with recursion

---
+++ HW18/RecursiveFunctions.js
@@ -0,0 +1,35 @@
+function factorial(n) {
+    if(n != 1){ 
+        return n * factorial(n - 1)
+        // 5 * factorial(4) 120
+        // 4 * factorial(3) 24
+        // 3 * factorial(2) 6
+        // 2 * factorial(1) 2
+    }else{
+        return 1;
+    }
+  }
+  
+console.log( factorial(5) ); 
+
+function sum(arr) {
+    if(arr.length === 0) {
+        return 0;
+    }else {
+        return arr[0] + sum(arr.slice(1));
+        //[13, 0, 4, 3, 9]
+        //13 + sum(arr.slice(1))
+        //[0, 4, 3, 9]
+        //0 + sum(arr.slice(1))
+        //[4, 3, 9]
+        //4 + sum(arr.slice(1))
+        //[3, 9]
+        //3 + sum(arr.slice(1))
+        //[9]
+        //9 + sum(arr.slice(1))
+        //[]
+    }
+}
+
+
+console.log(sum([13, 0, 4, 3, 9]));
