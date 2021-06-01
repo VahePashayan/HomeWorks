5c0192fe927b19bd390395a7a6b8725801815ca6
VahePashayan <vpashayan39gmail.com>
2021-06-01 12:41:27 +0400

    fix: added fibonacci function with recursion

--- HW18/RecursiveFunctions.js
+++ HW18/RecursiveFunctions.js
@@ -31,5 +31,17 @@ function sum(arr) {
     }
 }
 
-
 console.log(sum([13, 0, 4, 3, 9]));
+
+function fib(n) {
+    let arr
+    if(n === 1 || n === 2) {
+        return 1;
+    }else {
+        return fib(n-1) + fib(n-2);
+    }
+}
+
+console.log(fib(5));
+
+
